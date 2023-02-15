e659a543c5d0bd1bed9dc91779485175b1774c7f
Yury.Fedoseev <Yri95@inbox.ru>
2023-02-15 11:22:16 +0300

    First code

---
+++ src/test/java/guru/qa/TestConflict.java
@@ -0,0 +1,21 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+
+import java.util.Random;
+
+
+public class TestConflict {
+    @Test
+    public void conflict() {
+        int finalPeremen = 6;
+        int vvodPeremen = 0;
+
+        if (finalPeremen == vvodPeremen) return false;
+
+
+    }
+
+
+}
